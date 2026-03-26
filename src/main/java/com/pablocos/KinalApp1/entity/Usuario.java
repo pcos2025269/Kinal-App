6a371c047b2b7c4289a3d28ec4e247d7003c2c8c
pcos2025269 <pcos-2025269@kinal.edu.gt>
2026-03-25 22:45:04 -0600

    feat: UserRepository has been implemented in Kinal-App

---
+++ src/main/java/com/pablocos/KinalApp1/entity/Usuario.java
@@ -0,0 +1,85 @@
+package com.pablocos.KinalApp1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name = "Usuarios" )
+public class Usuario {
+    @Id
+    @Column(name = "codigo_Usuario", nullable = false)
+    private Long codigoUsuario;
+    @Column(nullable = false)
+    private String nameUser;
+    @Column(nullable = false)
+    private String passwordUser;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String rol;
+    @Column(nullable = false)
+    private Long estado;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long codigoUsuario, String nameUser, String passwordUser, String email, String rol, Long estado) {
+        this.codigoUsuario = codigoUsuario;
+        this.nameUser = nameUser;
+        this.passwordUser = passwordUser;
+        this.email = email;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    public Long getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(Long codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getPasswordUser() {
+        return passwordUser;
+    }
+
+    public void setPasswordUser(String passwordUser) {
+        this.passwordUser = passwordUser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Long getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Long estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/pablocos/KinalApp1/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.pablocos.KinalApp1.repository;
+
+import com.pablocos.KinalApp1.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+}
