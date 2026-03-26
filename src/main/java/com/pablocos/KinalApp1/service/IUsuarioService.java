69245bcafb2b29deffd18e7534ce7af2b8577d9c
pcos2025269 <pcos-2025269@kinal.edu.gt>
2026-03-25 23:19:23 -0600

    feat: add User Interface in Kinal-App

---
+++ src/main/java/com/pablocos/KinalApp1/service/IUsuarioService.java
@@ -0,0 +1,19 @@
+package com.pablocos.KinalApp1.service;
+
+import com.pablocos.KinalApp1.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService {
+
+    List<Usuario> listarTodos();
+
+    Usuario guardar(Usuario usuario);
+
+    Optional<Usuario> buscarPorEstado(Long estado);
+
+    Usuario actualizar(Long codigoUsuario, Usuario usuario);
+
+    void eliminar(Long codigoUsuario);
+}
