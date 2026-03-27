0e440cc25b56b63e30165cc18fddfffbfd68694d
pcos2025269 <pcos-2025269@kinal.edu.gt>
2026-03-26 21:40:09 -0600

    Add Product entity in Kinal-App

---
+++ src/main/java/com/pablocos/KinalApp1/entity/Producto.java
@@ -0,0 +1,75 @@
+package com.pablocos.KinalApp1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name ="Productos")
+public class Producto {
+    @Id
+    @Column(name ="codigo_producto")
+    private Long codigoProducto;
+    @Column
+    private String nombreProducto;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal precio;
+    @Column
+    private Long stock;
+    @Column
+    private String estado;
+
+    public Producto() {
+    }
+
+    public Producto(Long codigoProducto, String nombreProducto, BigDecimal precio, Long stock, String estado) {
+        this.codigoProducto = codigoProducto;
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public Long getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(Long codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
