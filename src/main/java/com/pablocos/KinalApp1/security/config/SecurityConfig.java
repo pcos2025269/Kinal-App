bec12e28a9f884c730ddf7813e6ca7899c165410
pcos-2025269 <pcos-2025269@kinal.edu.gt>
2026-04-29 23:38:20 -0600

    feat: added securityfiterChain into kinal app

--- src/main/java/com/pablocos/KinalApp1/security/config/SecurityConfig.java
+++ src/main/java/com/pablocos/KinalApp1/security/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.pablocos.KinalApp1.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain SecurityFilterChain (HttpSecurity http){
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/",true)
+                        .permitAll()
+
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                );
+        return  http.build();
+    }
+
+    
 }
