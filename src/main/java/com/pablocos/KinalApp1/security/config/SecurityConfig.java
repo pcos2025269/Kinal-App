17bd41280377854d5c82a0c049712e8be92d6db5
pcos-2025269 <pcos-2025269@kinal.edu.gt>
2026-05-01 17:23:36 -0600

    feat: added CustomUserDetails into SecurityConfig

--- src/main/java/com/pablocos/KinalApp1/security/config/SecurityConfig.java
+++ src/main/java/com/pablocos/KinalApp1/security/config/SecurityConfig.java
@@ -1,17 +1,28 @@
 package com.pablocos.KinalApp1.security.config;
 
 
+import com.pablocos.KinalApp1.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.net.http.HttpRequest;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain SecurityFilterChain (HttpSecurity http){
         http
