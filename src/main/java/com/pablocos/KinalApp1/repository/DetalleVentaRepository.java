636a909c96f9ec4cc992a6c809920257e2dd218c
pcos-2025269 <pcos-2025269@kinal.edu.gt>
2026-05-19 13:09:11 -0600

    Revert "Dev"

--- src/main/java/com/pablocos/KinalApp1/repository/DetalleVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pablocos.KinalApp1.repository;
-
-import com.pablocos.KinalApp1.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
--- src/main/java/com/pablocos/KinalApp1/repository/VentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pablocos.KinalApp1.repository;
-
-import com.pablocos.KinalApp1.entity.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
